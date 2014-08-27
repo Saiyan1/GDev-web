4c30999eda7f51591061c91cfbe0a382ae26f055
Iván Gabriel Sosa <ivangsosa@gmail.com>
2014-08-27 01:47:59 -0300

    Actualizadas librerías: Bootstrap a la v3.2.0, jQuery a la v2.1.1, jQuery UI a la v1.11.1, Font Awesome a la v4.1.0, HTML5 Shiv a la v3.7.2

--- assets/js/ajax/general_api.js
+++ assets/js/ajax/general_api.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
-	
-	// Establecer base_url
-	var hostname = location.hostname; // Puede ser "localhost" o "xx.com.ar"
-	var pathname = location.pathname;
-
-	window.base_url = 'http://' + hostname + '/';
 
 });
